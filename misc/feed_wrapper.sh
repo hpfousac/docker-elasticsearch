1f0ac581d5f749f41d3908255f56af79917291a7
HP Fousac <hpfousac@gmail.com>
2019-06-15 09:08:00 +0200

    another utility tool

---
+++ misc/feed_wrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for IFACE in xl0 re0 gif0 ;  do
+	HOUR=0
+	echo ${IFACE}
+	while [ ${HOUR} -lt 24 ] ; do
+		FILE_HOUR=`printf '%02d' ${HOUR}`
+		for JSON in `ls tcpdump-20190614-${FILE_HOUR}*-${IFACE}.json` ; do
+			echo feed -f ${JSON} -s 10.245.6.188 -i tcpdump
+		done
+		HOUR=`expr ${HOUR} + 1`
+	done
+done
+
