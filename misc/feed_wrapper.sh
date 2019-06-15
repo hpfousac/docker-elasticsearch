ea5b978e29f8735e90034005e04b70d9c027fafe
HP Fousac <hpfousac@gmail.com>
2019-06-15 09:09:29 +0200

    server become a parameter

--- misc/feed_wrapper.sh
+++ misc/feed_wrapper.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+ELASTIC_SERVER=10.245.6.188
+
 for IFACE in xl0 re0 gif0 ;  do
 	HOUR=0
 	echo ${IFACE}
 	while [ ${HOUR} -lt 24 ] ; do
 		FILE_HOUR=`printf '%02d' ${HOUR}`
 		for JSON in `ls tcpdump-20190614-${FILE_HOUR}*-${IFACE}.json` ; do
-			echo feed -f ${JSON} -s 10.245.6.188 -i tcpdump
+			echo feed -f ${JSON} -s ${ELASTIC_SERVER} -i tcpdump
 		done
 		HOUR=`expr ${HOUR} + 1`
 	done
