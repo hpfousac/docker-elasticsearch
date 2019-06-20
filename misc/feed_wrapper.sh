6f2511d2dcd2b9e7ded48a45fcd70065d46accb9
HP Fousac <hpfousac@gmail.com>
2019-06-20 10:37:32 +0200

    reorganized fork(s)

--- misc/feed_wrapper.sh
+++ misc/feed_wrapper.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-ELASTIC_SERVER=10.245.6.188
+ELASTIC_SERVER=10.245.6.201
+
+DATESTRING=$1
 
 for IFACE in xl0 re0 gif0 ;  do
 	HOUR=0
 	echo ${IFACE}
 	while [ ${HOUR} -lt 24 ] ; do
 		FILE_HOUR=`printf '%02d' ${HOUR}`
-		for JSON in `ls tcpdump-20190614-${FILE_HOUR}*-${IFACE}.json` ; do
-			echo feed -f ${JSON} -s ${ELASTIC_SERVER} -i tcpdump
+		for JSON in `ls tcpdump-${DATESTRING}-${FILE_HOUR}*-${IFACE}.json` ; do
+			./feed_bulk.py -f ${JSON} -s ${ELASTIC_SERVER} -i tcpdump-${DATESTRING} &
+			sleep 1
 		done
 		HOUR=`expr ${HOUR} + 1`
 	done
 done
+
+wait
 
