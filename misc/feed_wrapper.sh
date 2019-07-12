127e343cb0a20a319c5429f8dd50f9c2ddf08946
hpfousac@gmail.com <hpfousac@gmail.com>
2019-07-12 20:31:16 +0200

    hardcoded ingest pipeline name

--- misc/feed_wrapper.sh
+++ misc/feed_wrapper.sh
@@ -4,13 +4,15 @@ ELASTIC_SERVER=10.245.6.201
 
 DATESTRING=$1
 
+INGEST_PIPELINE=tcpdump_pipeline
+
 for IFACE in xl0 re0 gif0 ;  do
 	HOUR=0
 	echo ${IFACE}
 	while [ ${HOUR} -lt 24 ] ; do
 		FILE_HOUR=`printf '%02d' ${HOUR}`
 		for JSON in `ls tcpdump-${DATESTRING}-${FILE_HOUR}*-${IFACE}.json` ; do
-			./feed_bulk.py -f ${JSON} -s ${ELASTIC_SERVER} -i tcpdump-${DATESTRING} &
+			./feed_bulk.py -f ${JSON} -s ${ELASTIC_SERVER} -i tcpdump-${DATESTRING} --ingest-pipeline=tcpdump_pipeline &
 			sleep 1
 		done
 		HOUR=`expr ${HOUR} + 1`
@@ -18,4 +20,3 @@ for IFACE in xl0 re0 gif0 ;  do
 done
 
 wait
-
