42ff9ee99ce57b7d78738d3a91e83044e226376f
hpfousac@gmail.com <hpfousac@gmail.com>
2019-07-12 20:47:07 +0200

    added xz -d

--- misc/feed_wrapper.sh
+++ misc/feed_wrapper.sh
@@ -12,8 +12,10 @@ for IFACE in xl0 re0 gif0 ;  do
 	echo ${IFACE}
 	while [ ${HOUR} -lt 24 ] ; do
 		FILE_HOUR=`printf '%02d' ${HOUR}`
-		for JSON in `ls tcpdump-${DATESTRING}-${FILE_HOUR}*-${IFACE}.json` ; do
-			./feed_bulk.py -f ${JSON} -s ${ELASTIC_SERVER} -p ${ELASTIC_PORT} -i tcpdump-${DATESTRING} --ingest-pipeline=tcpdump_pipeline &
+		for JSON_XZ in `ls tcpdump-${DATESTRING}-${FILE_HOUR}*-${IFACE}.json.xz` ; do
+			JSON=`echo ${JSON_XZ} | sed -e 's/\.xz$//'`
+			xz -d < ${JSON_XZ} > /tmp/${JSON}
+			./feed_bulk.py -f /tmp/${JSON} -s ${ELASTIC_SERVER} -p ${ELASTIC_PORT} -i tcpdump-${DATESTRING} --ingest-pipeline=tcpdump_pipeline &
 			sleep 1
 		done
 		HOUR=`expr ${HOUR} + 1`
@@ -21,3 +23,7 @@ for IFACE in xl0 re0 gif0 ;  do
 done
 
 wait
+
+rm -f /tmp/tcpdump-${DATESTRING}-${FILE_HOUR}*.json
+
+exit 0
