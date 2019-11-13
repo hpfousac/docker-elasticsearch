a89cde7514b93a918e0f04c7924a9fe0579b6933
hpfousac@gmail.com <hpfousac@gmail.com>
2019-11-13 17:18:58 +0100

    elastic login

--- misc/gs2es.sh
+++ misc/gs2es.sh
@@ -23,11 +23,11 @@ gsutil ls ${GS_ROOT}'/tcpdump-*.xz' | while read GSNAME ; do
     YEAR=`echo ${YYYYMMDD} | cut -c1-4 | sed -e 's/^0*//'`
     MONTH=`echo ${YYYYMMDD} | cut -c5-6 | sed -e 's/^0*//'`
     DAY=`echo ${YYYYMMDD} | cut -c7-8 | sed -e 's/^0*//'`
-    gsutil cp ${GSNAME} ${BASENAME}
-    xz -d ${BASENAME}
-    ./tcpdump2es_v3.py -d ${DAY}.${MONTH}.${YEAR} -A -i tcpdump-v3- -b 200 -H gate -I ${IFACE} -f ${SHORTNAME}
-    rm -v ${SHORTNAME}
-    gsutil rm ${GSNAME}
+    echo gsutil cp ${GSNAME} ${BASENAME}
+    echo xz -d ${BASENAME}
+    echo ./tcpdump2es_v3.py -d ${DAY}.${MONTH}.${YEAR} -A -i tcpdump-v3- -b 10 -H gate -I ${IFACE} -f ${SHORTNAME}
+    echo rm -v ${SHORTNAME}
+    echo gsutil rm ${GSNAME}
 done
 
 wait
