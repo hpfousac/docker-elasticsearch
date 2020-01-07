600c123c3d394fc9a53ca9eaf5afa208136a48a8
david <hpfousac@gmail.com>
2020-01-07 18:37:58 +0100

    added CLI getopt

--- misc/fs2es.sh
+++ misc/fs2es.sh
@@ -4,14 +4,66 @@
 ## Load from GCE Storage to local ES
 ##
 
-TS_MASK=$1
-MONTH_MASK=`echo ${TS_MASK} | cut -c1-6`
-FS_ROOT=/server/stage/david/tcpdump/gate/incoming-data/${MONTH_MASK}
+# default values
+ADDITIONAL_OPTS=
 
-TMP=/home/david/ramdisk/tmp
+ES_SERVER=localhost
+ES_PORT=9200
+
+TMP_DIR=/home/david/ramdisk/tmp
+
+while getopts "c:vs:S:P:m:w:h?" opt; do
+  case $opt in
+    h)
+      usage
+      exit 0
+      ;;
+    c)
+      COLLECTOR_HOST=$OPTARG
+      info_msg COLLECTOR_HOST=${COLLECTOR_HOST}
+	  ;;
+    m)
+      DATETIME_STAMP=$OPTARG
+      info_msg DATETIME_STAMP=${DATETIME_STAMP}
+	  ;;
+    s)
+      SOURCE_DIR=$OPTARG
+      info_msg SOURCE_DIR=${SOURCE_DIR}
+      ;;
+    S)
+      ES_SERVER=$OPTARG
+      info_msg ES_SERVER=${ES_SERVER}
+      ;;
+    P)
+      ES_PORT=$OPTARG
+      info_msg ES_PORT=${ES_PORT}
+      ;;
+    v)
+      FLAG_VERBOSE=1
+	  ADDITIONAL_OPTS="${ADDITIONAL_OPTS} --verbose"
+      info_msg VERBOSE
+	  ;;
+    w)
+      TMP_DIR=$OPTARG
+      info_msg TMP_DIR=${TMP_DIR}
+	  ;;
+    \?)
+      # allow other args to pass on to mavproxy
+      usage
+      exit 0
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      exit 3
+  esac
+done
+
+MONTH_MASK=`echo ${DATETIME_STAMP} | cut -c1-6`
+FS_ROOT=/server/stage/david/tcpdump/${COLLECTOR_HOST}/incoming-data/${MONTH_MASK}
 
 
-ls ${FS_ROOT}/tcpdump-${TS_MASK}-*.xz | while read FNAME ; do
+ls ${FS_ROOT}/tcpdump-${DATETIME_STAMP}-*.xz | while read FNAME ; do
     BASENAME=`basename ${FNAME}`
     SHORTNAME=`echo ${BASENAME} | sed -e 's/\.xz$//'`
     IFACE=`echo ${SHORTNAME} | cut -d- -f4`
@@ -19,10 +71,10 @@ ls ${FS_ROOT}/tcpdump-${TS_MASK}-*.xz | while read FNAME ; do
     YEAR=`echo ${YYYYMMDD} | cut -c1-4 | sed -e 's/^0*//'`
     MONTH=`echo ${YYYYMMDD} | cut -c5-6 | sed -e 's/^0*//'`
     DAY=`echo ${YYYYMMDD} | cut -c7-8 | sed -e 's/^0*//'`
-    cp ${FNAME} ${TMP}/
-    xz -d ${TMP}/${BASENAME}
-    ./tcpdump2es_v3.py -d ${DAY}.${MONTH}.${YEAR} -A -i tcpdump-v3- -b 500 -H gate -I ${IFACE} -f ${TMP}/${SHORTNAME}
-    rm -v ${TMP}/${SHORTNAME}
+    cp ${FNAME} ${TMP_DIR}/
+    xz -d ${TMP_DIR}/${BASENAME}
+    ./tcpdump2es_v3.py -s ${ES_SERVER} -p ${ES_PORT} -d ${DAY}.${MONTH}.${YEAR} -A -i tcpdump-v3- -b 500 -H ${COLLECTOR_HOST} -I ${IFACE} ${ADDITIONAL_OPTS} -f ${TMP_DIR}/${SHORTNAME}
+    rm -v ${TMP_DIR}/${SHORTNAME}
 done
 
 wait
